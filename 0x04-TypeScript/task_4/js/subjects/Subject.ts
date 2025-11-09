4ea32a4cfe23e51e7ab1a9eb598c0e17e2af6b09
Reseanne Rampou <reseanner@gmail.com>
2025-11-09 18:53:29 +0200

    Fix Subject.ts: add setTeacher and teacher property correctly

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -2,12 +2,10 @@
 
 namespace Subjects {
   export class Subject {
-    teacher: Teacher;
+    teacher!: Teacher;
+
     setTeacher(teacher: Teacher): void {
       this.teacher = teacher;
     }
   }
 }
-
-
-
