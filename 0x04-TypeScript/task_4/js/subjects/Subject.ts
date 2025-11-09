97a33394526072671f080e80533d76178d5fa547
Reseanne Rampou <reseanner@gmail.com>
2025-11-09 18:05:20 +0200

    modified:   0x04-TypeScript/task_4/js/subjects/Subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -4,6 +4,7 @@ namespace Subjects {
   export class Subject {
     teacher: Teacher;
 
+    // ALX expects the setter method exactly like this
     setTeacher(teacher: Teacher): void {
       this.teacher = teacher;
     }
