fbf1b8a47ea4dedab0ed290b93b3a5d62bb2db7f
Reseanne Rampou <reseanner@gmail.com>
2025-11-09 19:02:21 +0200

    0x04-TypeScript/task_4/js/subjects/Subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -2,7 +2,7 @@
 
 namespace Subjects {
   export class Subject {
-    teacher!: Teacher;
+    teacher!: Teacher; // definite assignment for strict TypeScript
 
     setTeacher(teacher: Teacher): void {
       this.teacher = teacher;
