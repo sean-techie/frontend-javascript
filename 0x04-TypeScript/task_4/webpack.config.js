185a22f0f548499b15372f38b77fb4718d81daee
Reseanne Rampou <reseanner@gmail.com>
2025-11-09 17:33:50 +0200

    Add task_4 with namespace, declaration merging, and TypeScript setup

---
+++ 0x04-TypeScript/task_4/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/subjects/Cpp.ts', // you can change this to any main TS file
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
