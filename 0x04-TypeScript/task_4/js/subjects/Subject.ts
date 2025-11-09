1e1dbcd45aa7e21d099af09fb7ccfa7850c02d0c
Reseanne Rampou <reseanner@gmail.com>
2025-11-09 18:25:55 +0200

    Fix Subject.ts: correct namespace and setTeacher method

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -3,12 +3,11 @@
 namespace Subjects {
   export class Subject {
     teacher: Teacher;
-
-    // ALX expects the setter method exactly like this
     setTeacher(teacher: Teacher): void {
       this.teacher = teacher;
     }
   }
 }
+
 
 
